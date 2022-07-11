a5510af8d1df4e8dcd9b521482e94955f13d1f0c
zhongkai <zhongkai1010@163.com>
2022-07-11 23:05:11 +0800

    布局调整

--- src/layouts/complex.vue
+++ src/layouts/complex.vue
@@ -1,16 +1,16 @@
 <template>
   <el-container>
     <el-aside class="aside-wrapper">
-      <div class="logo-wrapper" />
-      <div class="menu-wrapper" />
+      <div class="logo-wrapper">logo-wrapper</div>
+      <div class="menu-wrapper">menu-wrapper</div>
     </el-aside>
-    <el-main>
+    <el-main class="main-wrapper">
       <el-container>
         <el-header class="header-wrapper">
-          <div class="header-wrapper">header-wrapper</div>
+          <div class="nav-wrapper">header-wrapper</div>
           <div class="tabs-wrapper">tabs-wrapper</div>
         </el-header>
-        <el-main class="main-wrapper"> main-wrapper </el-main>
+        <el-main class="body-wrapper"> main-wrapper </el-main>
       </el-container>
     </el-main>
   </el-container>
@@ -18,6 +18,9 @@
 
 <script setup lang="ts">
 import { mock } from "mockjs";
+import { computed } from "vue";
+import { siteConfigStoreHook } from "/@/store/globalConfig";
+const menuWidth = computed(() => `${siteConfigStoreHook().menuWidth}px`);
 const data = mock({
   "tabs|20": [
     {
@@ -48,7 +51,41 @@ console.log(data);
 </script>
 
 <style lang="scss" scoped>
+$logo-width: 64px;
+$header-height: 60px;
 .aside-wrapper {
   display: flex;
+  flex-direction: column;
+  width: v-bind(menuWidth);
+  .logo-wrapper {
+    height: $header-height;
+    width: v-bind(menuWidth);
+    background-color: #94b49f;
+  }
+  .menu-wrapper {
+    width: v-bind(menuWidth);
+    height: calc(100vh - $header-height);
+    background-color: #cee5d0;
+  }
+}
+.main-wrapper {
+  padding: 0;
+  .header-wrapper {
+    padding: 0;
+    height: calc($header-height + 50px);
+    .nav-wrapper {
+      width: calc(100vw - v-bind(menuWidth));
+      height: $header-height;
+      background-color: #fcf8e8;
+    }
+    .tabs-wrapper {
+      height: 50px;
+      width: calc(100vw - v-bind(menuWidth));
+      background-color: #ecb390;
+    }
+  }
+  .body-wrapper {
+    background-color: #94b49f;
+  }
 }
 </style>
