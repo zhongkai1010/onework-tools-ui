94f4d9e971e6d543e00a73dbca782c681c878c33
zhongkai1010 <zhongkai1010@163.com>
2022-07-15 17:14:02 +0800

    build

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,34 +1,3 @@
 <template>
-  <div class="ow-wrapper">
-    <el-container>
-      <el-aside class="aside-wrapper">
-        <el-menu :router="true" :mode="`vertical`" :collapse="true">
-          <template v-for="menu in menus" :key="menu.path">
-            <sub-menu :menu="menu" />
-          </template>
-        </el-menu>
-      </el-aside>
-      <el-main>Main</el-main>
-    </el-container>
-  </div>
+  <div>index</div>
 </template>
-
-<script setup lang="ts">
-import _ from "lodash";
-import { getModuleRoutes } from "/@/router/utils";
-import { getHeaderMenus } from "/@/layouts/utils";
-const routes = getModuleRoutes();
-const menus = getHeaderMenus(routes);
-</script>
-
-<style lang="scss" scoped>
-.ow-wrapper {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background-color: $header-background;
-  .aside-wrapper {
-    height: 100vh;
-  }
-}
-</style>
--- src/views/message/logs/index.vue
+++ src/views/message/logs/index.vue
@@ -1,28 +1,7 @@
 <template>
-  <div class="ow-wrapper">
-    <div class="tabs-wrapper">
-      <LayoutBreadcrumb
-        :data="[
-          { title: '首页', path: '/', icon: 'ant-design:home-outlined' },
-          { title: '示例', path: '/demo', icon: 'mdi:ev-plug-chademo' },
-          { title: '布局', path: '/demo/layout', icon: 'ant-design:layout-outlined' }
-        ]"
-        :show-fold="true"
-      />
-    </div>
-  </div>
+  <div>message logs</div>
 </template>
 
-<script lang="ts" setup>
-import LayoutBreadcrumb from "/@/layouts/components/breadcrumb/index.vue";
-</script>
+<script setup lang="ts"></script>
 
-<style lang="scss" scoped>
-.ow-wrapper {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  .tabs-wrapper {
-  }
-}
-</style>
+<style scoped></style>
