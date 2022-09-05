b4e6f3a56593126266ebf664cf5fc89d08576c55
zhongkai1010 <zhongkai1010@163.com>
2022-09-05 16:03:33 +0800

    1

---
+++ src/views/demo/form/custom/handle.ts
@@ -0,0 +1,33 @@
+// import _ from 'lodash';
+// import { Property } from './types';
+
+// export default function (data: Property[]) {
+//   const properties = ref<Property[]>(data);
+
+//   const result = computed(() => {
+//     return _convert(list.value, undefined);
+//   });
+
+//   const _convert = (value: Property[], id: string) => {
+//     const result = [];
+//     const childs = values.filter((t) => t. == parent);
+//     childs.forEach((t) => {
+//       const node = t;
+//       node[childKey] = _convert(value, t);
+//       result.push(node);
+//     });
+//     return result;
+//   };
+
+//   const addSameNode = (node: T) => {};
+
+//   const addChildNode = (node: T) => {};
+
+//   const remove = (node: T) => {};
+
+//   return {
+//     result,
+//     add,
+//     remove,
+//   };
+// }
