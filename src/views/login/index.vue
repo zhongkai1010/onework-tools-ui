a906d2234f57a2acf53a119df00d7c3e1a715c77
zhongkai1010 <zhongkai1010@163.com>
2022-11-28 18:00:35 +0800

    登录样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,60 @@
 <template>
-  <el-button type="primary" @click="onClickLogin" :loading="loading">
-    {{ $t('app.login.loginButton') }}
-  </el-button>
+  <div class="form_wrapper">
+    <img :src="logoImg" />
+    <h2>OneWork Tools UI</h2>
+    <form>
+      <el-form-item>
+        <el-input v-model="formValue.username" clearable>
+          <template #prefix>
+            <IconifyIcon icon="ant-design:user-outlined" />
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="formValue.password" type="password" clearable show-password>
+          <template #prefix>
+            <IconifyIcon icon="ant-design:lock-filled" />
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="formValue.validateCode" clearable>
+          <template #prefix>
+            <IconifyIcon icon="ant-design:security-scan-filled" />
+          </template>
+          <template #append>
+            <canvas width="120" height="40" class="cursor-pointer"></canvas>
+          </template>
+        </el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-checkbox label="记住密码" v-model="formValue.remember" />
+        <el-button link>忘记密码？</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onClickLogin" :loading="loading" style="width: 100%">
+          {{ $t('app.login.loginButton') }}
+        </el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-space>
+          <el-button>手机登录</el-button>
+          <el-button>二维码登录</el-button>
+          <el-button>注册</el-button>
+        </el-space>
+      </el-form-item>
+      <el-form-item>
+        <el-divider content-position="left">第三方登录</el-divider>
+        <el-space>
+          <IconifyIcon icon="ant-design:lock-filled" />
+          <IconifyIcon icon="ant-design:lock-filled" />
+          <IconifyIcon icon="ant-design:lock-filled" />
+          <IconifyIcon icon="ant-design:lock-filled" />
+        </el-space>
+      </el-form-item>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -14,9 +67,17 @@
   import { getNavRecordRaw, getNavRecordRawMap } from '/@/layouts/utils';
   import commonApi from '/@/api/common';
   import { useRouter } from 'vue-router';
+  import logoImg from '/@/assets/logo.png';
 
   const router = useRouter();
   const loading = ref(false);
+
+  const formValue = reactive({
+    username: '',
+    password: '',
+    validateCode: '',
+    remember: false
+  });
 
   // 已登录，跳转首页
 
@@ -55,4 +116,25 @@
   };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .form_wrapper {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    background-color: #fff;
+    width: 460px;
+    margin-left: 40%;
+    padding: 20px;
+    img {
+      height: 80px;
+      align-items: center;
+    }
+    h2 {
+      color: #999;
+      font: 700 200% Consolas, Monaco, monospace;
+      margin: 15px 0;
+      text-transform: uppercase;
+    }
+  }
+</style>
