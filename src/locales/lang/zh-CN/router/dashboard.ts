a5a03e10c791b1211058f4a6f2b9cbc434ca8d1d
zhongkai1010 <zhongkai1010@163.com>
2022-11-30 15:13:28 +0800

    中文语言文件

---
+++ src/locales/lang/zh-CN/router/dashboard.ts
@@ -0,0 +1,6 @@
+export default {
+  dashboard: '工作台',
+  analysis: '分析页',
+  monitor: '监控页',
+  workplace: '工作台',
+};
---
+++ src/locales/lang/zh-CN/router/system.ts
@@ -0,0 +1,21 @@
+export default {
+  system: '系统',
+  page: '页面管理',
+  user: '用户管理',
+  role: '角色管理',
+  menu: '菜单管理',
+  department: '部门管理',
+  post: '岗位管理',
+  dictionary: {
+    dictionary: '数据字典',
+    single: '单项字典',
+    multiple: '多项字典',
+  },
+  config: '参数设置',
+  notice: '通知公告',
+  log: {
+    log: '日志管理',
+    operate: '操作日志',
+    login: '登录日志',
+  },
+};
