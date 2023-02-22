ddacb03efdf72de61c78cb0f56dd7bb0b991031a
zhongkai <zhongkai1010@163.com>
2023-02-22 19:31:42 +0800

    1

--- src/api/site/team/index.ts
+++ src/api/site/team/index.ts
@@ -1,7 +1,34 @@
-import { PageResult } from '/@/api/common';
 import { http } from '/@/utils/http/axios';
+import { RequestResult, ResponseData, ResponsePageData } from '/@/api/types';
+
+export interface GetListSearchParams {
+  userName: string;
+}
+
+export interface SiteTeamPersonnel {
+  userName: string;
+}
+
+export interface SiteProfessional {
+  dempName: string;
+}
 
 export default {
-  getList: () => http.get<PageResult<any>>({ url: '/site/team/join/getList' }),
+  /**
+   *  获取机构团队人员列表
+   * @returns
+   */
+  getList: (params: GetListSearchParams) =>
+    http.get<RequestResult<ResponsePageData<SiteTeamPersonnel>>>({
+      url: '/site/team/join/getList',
+      params
+    }),
+
+  getSiteProfessionalList: () =>
+    http.get<RequestResult<ResponseData<SiteProfessional>>>({
+      url: '/site/team/join/getSiteProfessionalList',
+      params
+    }),
+
   reviewJoin: () => http.get<PageResult<any>>({ url: '/site/team/join/reviewJoin' })
 };
